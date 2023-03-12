66d96ffca35664c092a380226b9ec60c20d9b58b
Abdurahman Tumgaev <abdurahman.tumgaev@gmail.com>
2023-03-12 16:47:26 +0100

    tset

--- index.ts
+++ index.ts
@@ -1183,8 +1183,8 @@ client.on('interactionCreate', (interaction: Interaction) => {
       usernameAndId.length,
       interaction.customId.length
     );
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    const func = buttonIdsToFunctions[buttonTypeId as any];
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-explicit-any, @typescript-eslint/no-unsafe-member-access
+    const func: any = buttonIdsToFunctions[buttonTypeId as any];
     if (func != null) {
       // eslint-disable-next-line max-depth, @typescript-eslint/no-unsafe-call
       if (func(interaction, usernameAndId) == true) {
